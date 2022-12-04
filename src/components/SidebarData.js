4f214a929971aeefb5b0c990fdb4a6bca927c704
tkawabey <tkawabuchi@fournonblonds.sakura.ne.jp>
2022-12-04 22:20:37 +0900

    first commit

---
+++ src/components/SidebarData.js
@@ -0,0 +1,50 @@
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import AttachEmailIcon from "@mui/icons-material/AttachEmail";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import AddCardIcon from "@mui/icons-material/AddCard";
+import BackupIcon from "@mui/icons-material/Backup";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+/*
+  サイドバーに表示するデータ作成
+  Jsonデータとして作成
+*/
+export const SidebarData = [
+  {
+    title: "ホーム",
+    icon: <HomeIcon />,
+    link: "/home",
+  },
+  {
+    title: "メール",
+    icon: <AttachEmailIcon />,
+    link: "/mail",
+  },
+  {
+    title: "アナリティクス",
+    icon: <AssessmentIcon />,
+    link: "/analitics",
+  },
+  {
+    title: "友達追加",
+    icon: <PersonAddIcon />,
+    link: "/friends",
+  },
+  {
+    title: "お支払い設定",
+    icon: <AddCardIcon />,
+    link: "/payment",
+  },
+  {
+    title: "アップロード",
+    icon: <BackupIcon />,
+    link: "/upload",
+  },
+  {
+    title: "詳細設定",
+    icon: <SettingsIcon />,
+    link: "/rocket",
+  },
+];
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from '../logo.svg';
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={logo} />
+      <p>hogehoge@mail.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
